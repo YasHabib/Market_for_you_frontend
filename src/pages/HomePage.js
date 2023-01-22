3f1db6b072539289cae9ed63df2ba6c7bc611717
YasHabib <yasin_habib@outlook.com>
2023-01-22 14:28:56 -0700

    Added assets for home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,26 +7,27 @@ export default function Dashboard() {
         <header className='Header'>
           <h1>Header</h1>
         </header>
-        <div className='DealsForYou'>
-          <h1>DealsForYou</h1>
-        </div>
-        <span className='CategoryBlock'>
-          <h1>Category 1</h1>
-        </span>
-        <span className='CategoryBlockSide'>
-            <h1>Category 2</h1>
-        </span>
-        <div className='MoreDealsForYou'>
-            <h1>More DealsForYou</h1>
-        </div>
-        <span className='CategoryBlock'>
-            <h1>Category 3</h1>
-        </span>
-        <span className='CategoryBlockSide'>
-            <h1>Category 4</h1>
-        </span>
-        <div className='AppStoreLink'>
-            <h1>App Store Link</h1>
+        <div className='Wrapper'>
+          <div className='DealsForYou'>
+            <h1>DealsForYou</h1>
+          </div>
+          <span className='CategoryBlock'>
+            <h1>Category 1</h1>
+          </span>
+          <span className='CategoryBlockSide'>
+              <h1>Category 2</h1>
+          </span>
+          <div className='MoreDealsForYou'>
+              <h1>More DealsForYou</h1>
+          </div>
+          <span className='CategoryBlock'>
+              <h1>Category 3</h1>
+          </span>
+          <span className='CategoryBlockSide'>
+              <h1>Category 4</h1>
+          </span>
+          <div className='AppStoreLink'>
+          </div>
         </div>
         <footer>
           <h1>footer</h1>
